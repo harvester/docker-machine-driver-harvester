9343033c41e95121d8b623bd1d5cbebc18745690
futuretea <1913508671@qq.com>
2021-06-17 16:26:02 +0800

    remove vendor
    Signed-off-by: futuretea <1913508671@qq.com>

--- vendor/github.com/guonaihong/gout/dataflow/global_config.go
+++
@@ -1,8 +0,0 @@
-package dataflow
-
-import (
-	"github.com/guonaihong/gout/setting"
-)
-
-// 存放全局配置选项
-var GlobalSetting setting.Setting
--- vendor/github.com/guonaihong/gout/setting/setting.go
+++
@@ -1,8 +0,0 @@
-package setting
-
-// 设置
-type Setting struct {
-	NotIgnoreEmpty bool //控制是否使用空值
-
-	NoAutoContentType bool
-}
